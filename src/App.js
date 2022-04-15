638d3b61d789537a90ecc18ea3d763bff7588856
HyeonSu <cow549@naver.com>
2022-04-15 21:49:51 +0900

    과제

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Bowling({name, picture}) {
+  return (
+  <div>
+    <h1>I like {name}</h1>
+    <img src={picture} />
+  </div>
+  );
+}
+
+const bowlingILike = [
+  {
+    name: 'Roto Grip UC3',
+    image: 'http://happybowling.co.kr/web/product/big/202111/f2772e27804d31d4bcbe7bf22dc3ebba.jpg'
+  },
+  {
+    name: 'Stom PITCHBLACK',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.3T1rkT912TG3cOOfFeH_tgHaGL%26pid%3DApi&f=1'
+  },
+  {
+    name: 'Hammer PURPLEPEARL',
+    image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.FGgQUqqH3PxZ64era-Z9KQHaHa%26pid%3DApi&f=1'
+  },
+  {
+    name: '900Global HONEYBADHER',
+    image: 'http://openimage.interpark.com/goods_image/9/8/3/3/8776509833s.jpg'
+  },
+  {
+    name: 'Track PARDOX',
+    image: 'http://img.danawa.com/prod_img/500000/125/135/img/12135125_1.jpg?shrink=330:330&_v=20211216090923'
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    {bowlingILike.map(dish => <Bowling key={dish.name} Bowling name={dish.name} picture={dish.image} />)}
+  </div>
   );
 }
 
